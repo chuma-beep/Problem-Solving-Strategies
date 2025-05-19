34f3fd47d7d38468f7e305427833a6852c2eb0f4
chuma-beep <wisboynelson123@gmail.com>
2025-05-19 17:04:29 +0100

    added firt div & Conq example

--- DataStructures and Algorithms/divideandconquer/DivConq.py
+++ DataStructures and Algorithms/divideandconquer/DivConq.py
@@ -1,4 +1,18 @@
 # Divide and conquer algorithm to find the maximum number in a list 
 
 
-def maximumNum 
+def find_maximum(arr):
+    if len(arr) == 1:
+        return arr[0] # base case 
+
+    mid = len(arr) //2 
+    left_maximum = find_maximum(arr[:mid])
+    right_maximum = find_maximum(arr[mid:])
+
+    return max(left_maximum, right_maximum)
+
+a = [10, 12,43, 55, 22, 33, 45, 90]
+
+
+print(find_maximum(a))
+
